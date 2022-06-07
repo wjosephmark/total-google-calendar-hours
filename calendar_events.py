109bf3b067b8ad8a44d289ac15dc30d4e4fcac19
wjosephmark <wjosephmark@gmail.com>
2022-06-06 23:33:00 -0600

    created get_calendar_events function

--- calendar_events.py
+++ calendar_events.py
@@ -7,11 +7,17 @@ load_dotenv(find_dotenv())
 api_key = os.getenv('API_KEY')
 calendar_id = os.getenv('CALENDAR_ID')
 
-def main():
+
+def get_calendar_events():
     calnders_api_response = requests.get(f'https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events?key={api_key}')
 
     events_dictionary = calnders_api_response.text
 
+    return events_dictionary
 
-if __name__ = '__main__':
+def main():
+    events_dictionary = get_calendar_events()
+
+
+if __name__ == '__main__':
     main()
