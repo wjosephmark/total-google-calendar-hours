324d49fc376e76ab180a148c4825a8a7a63c9d04
Joseph Mark <jmark@socialclimb.com>
2022-07-13 21:01:24 -0600

    added basic logic for total hour determining
    added logic to determine hours and minutes

--- calendar_events.py
+++ calendar_events.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv, find_dotenv
+import datetime
 
 load_dotenv(find_dotenv())
 
@@ -9,15 +10,61 @@ calendar_id = os.getenv('CALENDAR_ID')
 
 
 def get_calendar_events():
-    calnders_api_response = requests.get(f'https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events?key={api_key}')
+    calenders_api_response = requests.get(f'https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events?key={api_key}')
 
-    events_dictionary = calnders_api_response.text
+    return calenders_api_response.json()
 
-    return events_dictionary
+def sort_calendar_events(events_dict):
+    relevant_events = []
+    total_hours_dict = {}
+    response_array = []
+    for event in events_dict:
+        # IF EVENT HAS TITLE
+        if 'summary' in event:
+            if 'Test' in event['summary']:
+                relevant_events.append(event)
+
+    for event in relevant_events:
+        summary = event['summary']
+
+        # SET UTC TIMESTAMPS TO VARIABLES
+        start_timestamp = event['start']['dateTime']
+        end_timestamp= event['end']['dateTime']
+
+        # CONVERT UTC TIMESTAMP TO DATETIME STRING
+        start_datetime = datetime.datetime.strptime(start_timestamp, '%Y-%m-%dT%H:%M:%S%z')
+        end_datetime = datetime.datetime.strptime(end_timestamp, '%Y-%m-%dT%H:%M:%S%z')
+
+        # EXTRACT HOURS AND MINUTES FROM DATETIME STRINGS
+        start_hour = start_datetime.hour
+        end_hour = end_datetime.hour
+        start_minute = start_datetime.minute
+        end_minute = end_datetime.minute
+
+        # FIND DIFFERENCE IN HOURS AND MINUTES
+        hour_diff = end_hour-start_hour
+        minute_diff = end_minute-start_minute
+
+        if summary not in total_hours_dict:
+            formatted_dict = {'summary': summary, 'hours': hour_diff, 'minutes': minute_diff}
+            total_hours_dict[summary] = formatted_dict
+        else:
+            total_hours_dict[summary]['hours'] = total_hours_dict[summary]['hours'] + hour_diff
+            total_hours_dict[summary]['minutes'] = total_hours_dict[summary]['minutes'] + minute_diff
+
+    for client in total_hours_dict:
+        client_info = total_hours_dict[client]
+        #TODO: ADD LOGIC TO DETERMINE USE CORRECT VARIATION OF MINUTES/HOURS
+        response_array.append(f"{client_info['summary']} owes for {client_info['hours']} hours and {client_info['minutes']} minutes")
+
+    return response_array
 
 def main():
-    events_dictionary = get_calendar_events()
-
+    reponse_dict = get_calendar_events()
+    events_dict = reponse_dict['items']
+    response = sort_calendar_events(events_dict)
+    for response_string in response:
+        print(response_string)
 
 if __name__ == '__main__':
     main()
